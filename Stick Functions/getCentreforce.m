4c8f07b414c5f1a74c3313eec6a69bf0ae8f58a6
aa8764 <161346019+aa8764@users.noreply.github.com>
2024-03-11 10:54:42 +0000

    Delete Stick Functions directory

--- Stick Functions/getCentreforce.m
+++
@@ -1,9 +0,0 @@
-function [Xf_loc , Yf_loc] = getCentreforce(stick_xpos,stick_ypos,stick_def)
-
-
-% find the closest value of x from X
-    [~,Xf_loc] = (min(abs(stick_def - stick_xpos)));
-        
-    % find the closest value of x from Y
-    [~,Yf_loc] = (min(abs(stick_def - stick_ypos)));
-    
